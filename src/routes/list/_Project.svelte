acf33c588093195988554c2a0cc39e79d82150aa
Leo Mihalcea <leomihalcea@gmail.com>
2022-04-23 21:43:33 +0300

    [#1] Create Project List Component and a lot of styling

---
+++ src/routes/list/_Project.svelte
@@ -0,0 +1,48 @@
+<script>
+	import { onMount } from 'svelte';
+
+	import Card, { Content, Actions } from '@smui/card';
+	import Chip, { Set, Text } from '@smui/chips';
+
+	export let id;
+	export let name;
+	export let technologies = [];
+	export let votes;
+
+	onMount(() => {
+		console.log(name);
+	});
+</script>
+
+<Card>
+	<Content>
+		<p>{name}</p>
+	</Content>
+
+	<div class='votes mdc-theme--secondary-bg'>
+		{votes}
+	</div>
+
+	<Actions>
+		<Set chips={technologies.slice(0, 3)} let:chip nonInteractive>
+			<Chip {chip}>
+				<Text>{chip}</Text>
+			</Chip>
+		</Set>
+	</Actions>
+</Card>
+
+<style>
+    p {
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+
+    .votes {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        color: gray; /*todo: find out how to get accent color from theme*/
+    }
+</style>
